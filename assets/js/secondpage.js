257bccec1d4d17ac490b6206e7f4b713d5707501
thedorktrain <mackemosabe@gmail.com>
2024-04-16 14:55:42 -0500

    added working Alien Chatbot

--- assets/js/secondpage.js
+++ assets/js/secondpage.js
@@ -20,4 +20,43 @@ generatePickupLineButt.addEventListener('click', function (){
     p.textContent= `Are you a telepathic being? Because I can't seem to get you out of my mind, no matter how hard I try`
 });
 
+const chatButt = document.querySelector("#chatButt");
+const dateChat = document.querySelector("#response");
+const chatInput = document.querySelector("#chatInput");
+const lastChat = document.querySelector("#lastChat");
 
+chatButt.addEventListener("click", function() {
+     lastChat.textContent = chatInput.value;
+    // const url2 = 'https://chatgpt-42.p.rapidapi.com/conversationgpt4';
+    // const options2 = {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //         'X-RapidAPI-Key': '5e481eaddcmsh660cb988e789db3p146e62jsn1eb1536b3f7f',
+    //         'X-RapidAPI-Host': 'chatgpt-42.p.rapidapi.com'
+    //     },
+    //     body: JSON.stringify({
+    //         messages: [
+    //             {
+    //                 role: 'user',
+    //                 content: `${chatInput.value}`
+    //             }
+    //         ],
+    //         system_prompt: `You are an alien on a date with me`,
+    //         temperature: 0.9,
+    //         top_k: 5,
+    //         top_p: 0.9,
+    //         max_tokens: 256,
+    //         web_access: false
+    //     })
+    // };
+
+    // fetch(url2, options2)
+    // .then(function(response) {
+    //     return response.json();
+    // })
+    // .then(function(data) {
+    //     dateChat.textContent = `${data.result}`
+    // });
+    dateChat.textContent = `Your Chat Partner is busy right now, but they will get back to you in a Galactic Minute (One Month)`
+});
