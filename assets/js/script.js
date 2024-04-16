ed6dca1da50f8deeae5b812a80c338721e81c39b
lindseymaddox9 <153481027+lindseymaddox9@users.noreply.github.com>
2024-04-16 12:48:57 -0500

    initial commit with basic code.Modal working

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+const heartButt = document.querySelector("#heartButt");
+
+heartButt.addEventListener('click', function () {
+console.log("he touched the Heartbutt")
+  // 1) Open the Modal
+  // 2) Save Information to Local Storage
+  // 3) Navigate to the Second Page
+});
+
+function openModal($el) {
+  $el.classList.add('is-active');
+}
+
+(document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+  const modal = $trigger.dataset.target;
+  const $target = document.getElementById(modal);
+
+  $trigger.addEventListener('click', () => {
+    openModal($target);
+  });
+});
